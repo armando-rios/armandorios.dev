d0daa115118d78ef790265461e666479800b7fe6
Armando Rios <armandorios.dev@gmail.com>
2024-11-03 17:59:39 -0800

    Add base color type catppuccin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        "primary-dark": "#fb7185",
+        base: "#1e1e2e",
+        mantle: "#181825",
+        crust: "#11111b",
+        text: "#cdd6f4",
+        subtext: "#a6adc8",
+        overlay0: "#6c7086",
+        surface0: "#313244",
+        surface1: "#45475a",
+        blue: "#89b4fa",
+        lavender: "#b4befe",
+        sapphire: "#74c7ec",
+        sky: "#89dceb",
+        mauve: "#cba6f7",
+        green: "#a6e3a1",
+        yellow: "#f9e2af",
       },
     },
   },
